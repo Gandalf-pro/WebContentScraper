cba7c62531b5d82e96689378179a7f01eef60927
Gandalf <asoc04@hotmail.com>
2019-12-04 01:07:57 +0300

    ful imdb integration

--- Imdb.js
+++ Imdb.js
@@ -6,6 +6,47 @@ class Imdb {
         this.URL = "https://www.imdb.com/title/";
     }
 
+    //yyyy-mm-dd
+    convertTime(time) {
+        time = time.replace(".", "");
+        time = time.trim();
+        let Dates = [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec"
+        ];
+        let pieces = time.split(" ");
+        //year
+        let year = pieces[2];
+        //month
+        let month =
+            Dates.findIndex((val, i, obj) => {
+                if (val == pieces[1]) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }) + 1;
+        //day
+        let day = pieces[0];
+        let actual = `${year}-${month}-${day}`;
+        return actual;
+    }
+
+    /**
+     *
+     * @param {String} imdbId
+     * returns return object
+     */
     async getShow(imdbId) {
         let url = this.URL + imdbId;
         let reqPage = await rp(url);
@@ -28,14 +69,71 @@ class Imdb {
         return ret;
     }
 
+    /**
+     *
+     * @param {String} imdbId
+     * @param {String} season
+     * @returns {String} return the number of episodes on a given season
+     */
     async getSeason(imdbId, season) {
         let url = this.URL + imdbId + `/episodes?season=${season}`;
+        let reqPage = await rp(url);
+        let page = cheerio.load(reqPage);
+        let episodeCount = page('meta[itemprop="numberofEpisodes"]').attr(
+            "content"
+        );
+
+        let haha = page('div[class="list detail eplist"]').html();
+        console.log(haha);
+
+        return episodeCount;
     }
 
-    async getEpisode(episodeImdbId) {
+    /**
+     * 
+     * @param {String} imdbId 
+     * @param {Number} season 
+     * returns every episode on a season
+     */
+    async getAllEpisodesBySeason(imdbId, season) {
+        let url = this.URL + imdbId + `/episodes?season=${season}`;
+        let reqPage = await rp(url);
+        let page = cheerio.load(reqPage);
+        let episodes = page('div[class="list detail eplist"]')
+            .children()
+            .toArray();
+        let returnArray = [];
 
+
+        for (const weird of episodes) {
+            let episode = cheerio.load(cheerio.html(weird));
+            let date = episode('div[class="airdate"]').text();
+            date = this.convertTime(date);
+            let obj = {
+                imdbId: imdbId,
+                episode: episode('meta[itemprop="episodeNumber"]').attr(
+                    "content"
+                ),
+                episodeImdbId: episode(
+                    'div[class="hover-over-image zero-z-index "]'
+                ).attr("data-const"),
+                summary: episode('div[itemprop="description"]').text().trim(),
+                rating: episode(
+                    'div[class="ipl-rating-widget"] span[class="ipl-rating-star__rating"]'
+                ).html(),
+                season: season,
+                dateReleased: date,
+                name: episode('a[itemprop="name"]').attr("title"),
+                posters: episode('div[class="image"] img').attr("src")
+            };
+            returnArray.push(obj);
+        }
+
+        return returnArray;
     }
+
+    async getEpisode(episodeImdbId) {}
 }
 
 let a = new Imdb();
-a.getShow("tt0108778");
+a.getAllEpisodesBySeason("tt0108778", 9);
